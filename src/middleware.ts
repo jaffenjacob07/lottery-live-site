32445c950ecf3f56efa5a01df34bee68b6265931
jaffenjacob07 <147614455+jaffenjacob07@users.noreply.github.com>
2026-05-28 14:27:16 +0530

    protect admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  const supabase = createClient(request);
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (request.nextUrl.pathname.startsWith("/admin")) {
-    if (!user) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-  }
-
-  return response;
+  return await createClient(request);
 }
 
 export const config = {
